e9acb5363de58acd7c71b5ea900f85d83ba20709
MistrDev <cwcorster123@gmail.com>
2021-05-14 16:12:12 -0500

    defined 3 routes for incoming request

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
  const express = require("express");
  const app = express()
 
-app.use((req, res) => {
-    console.log("I got a new request!")
-    res.send('<h1>This is a response rendered with HTML</h1>')
+// app.use((req, res) => {
+//console.log("I got a new request!")
+  //res.send('<h1>This is a response rendered with HTML</h1>')}) 
+
+app.get('/', (req, res) => {
+    res.send('HOME')
+})
+
+
+app.get('/cats', (req, res) => {
+    res.send('meow')
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('woof')
 })
 
  app.listen(3000, () => {
