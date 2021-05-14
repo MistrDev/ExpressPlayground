357fde130b05a343af052da48b56117019217f83
MistrDev <cwcorster123@gmail.com>
2021-05-14 16:03:56 -0500

    messing around with responses and request

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
  const express = require("express");
  const app = express()
 
-app.use(() => {
+app.use((req, res) => {
     console.log("I got a new request!")
+    res.send ({color: 'red'})
 })
 
  app.listen(3000, () => {
-     console.log('Listening on port 8080!')
+     console.log('Listening on port 3000!')
  })
